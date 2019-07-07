e98f7bd119dd26308a7c7f4cc7ce2dba2357156b
wsq <mywsq961@gmail.com>
2019-07-07 23:17:55 +0800

    feat: init

--- plugins/axios.js
+++
@@ -1,3 +0,0 @@
-export default function ({ $axios, redirect,env }) {
-    $axios.setHeader('X-Auth-Token', env.YUQUE_TOKEN)
-  }
--- plugins/day.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import 'dayjs/locale/zh-cn';
-import dayjs from 'dayjs';
-import LocalizedFormat from 'dayjs/plugin/localizedFormat'
-
-dayjs.locale('zh-cn');
-dayjs.extend(LocalizedFormat)
-
-Vue.prototype.$day = dayjs
