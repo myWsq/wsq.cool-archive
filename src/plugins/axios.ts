a068fc083abcb2421efbac6cadae2dd8ce7c5dbe
myWsq <wsq961@outlook.com>
2019-07-09 15:24:39 +0800

    Revert "fix: 为生产模式添加baseurl"
    This reverts commit 1b8c942cf5040ab97f0bef928041e8460d085d49.

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,5 +1,3 @@
 import Axios from "axios";
 
-export const AxiosDefault = Axios.create({
-  baseURL: process.env.NODE_ENV === "production" && process.env.VUE_APP_BASE_URL
-});
+export const AxiosDefault = Axios.create({});
