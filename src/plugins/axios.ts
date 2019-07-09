1b8c942cf5040ab97f0bef928041e8460d085d49
myWsq <wsq961@outlook.com>
2019-07-09 15:18:37 +0800

    fix: 为生产模式添加baseurl

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,3 +1,5 @@
 import Axios from "axios";
 
-export const AxiosDefault = Axios.create({});
+export const AxiosDefault = Axios.create({
+  baseURL: process.env.NODE_ENV === "production" && process.env.VUE_APP_BASE_URL
+});
